97039f28086b0bd88b5fc0e4e6bd10806eb505dd
samir <mandalsam@outlook.com>
2026-08-01 00:16:20 +0300

    Edit App.jsx to display from  array of application objects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,40 @@
 function App() {
+  const applications = [
+    {
+      company: 'Anthropic',
+      role: 'Software Engineer Intern',
+      dateApplied: '12/07/2026',
+      status: 'Pending',
+      deadline: '17/07/2026'
+    },
+    {
+      company: 'Microsoft',
+      role: 'Data Analyst',
+      dateApplied: '02/05/2026',
+      status: 'Pending',
+      deadline: '17/05/2026'
+    },
+    {
+      company: 'Acturis',
+      role: 'Integrations Developer',
+      dateApplied: '19/05/2026',
+      status: 'Pending',
+      deadline: '01/07/2026'
+    }
+  ]
+
   return (
     <>
       <h1>Job Tracker</h1>
       <p>A simple app to track job applications and their status.</p>
+
+      <div>
+        <h2>{applications[0].company}</h2>
+        <p>{applications[0].role}</p>
+        <p>{applications[0].dateApplied}</p>
+        <p>{applications[0].status}</p>
+        <p>{applications[0].deadline}</p>
+      </div>
     </>
   )
 }
