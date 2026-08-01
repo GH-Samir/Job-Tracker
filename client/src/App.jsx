7809ff2a4971db894685c908fe7974e31b18b5d2
samir <mandalsam@outlook.com>
2026-08-01 11:57:21 +0300

    Add mapping to Application Cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import ApplicationCard from "./ApplicationCard"
 function App() {
   const applications = [
     {
+      id: 0,
       company: 'Anthropic',
       role: 'Software Engineer Intern',
       dateApplied: '12/07/2026',
@@ -11,6 +12,7 @@ function App() {
       deadline: '17/07/2026'
     },
     {
+      id: 1,
       company: 'Microsoft',
       role: 'Data Analyst',
       dateApplied: '02/05/2026',
@@ -18,11 +20,20 @@ function App() {
       deadline: '17/05/2026'
     },
     {
+      id: 2,
       company: 'Acturis',
       role: 'Integrations Developer',
       dateApplied: '19/05/2026',
       status: 'Pending',
       deadline: '01/07/2026'
+    },
+    {
+      id: 3,
+      company: 'Google',
+      role: 'Security consultant',
+      dateApplied: '11/08/2026',
+      status: 'Pending',
+      deadline: '24/09/2026'
     }
   ]
 
@@ -30,10 +41,7 @@ function App() {
     <>
       <h1>Job Tracker</h1>
       <p>A simple app to track job applications and their status.</p>
-          
-      <ApplicationCard application={applications[0]} />
-      <ApplicationCard application={applications[1]} />
-      <ApplicationCard application={applications[2]} />
+      {applications.map((application) => <ApplicationCard key={application.id} application={application}/>)}
     </>
   )
 }
