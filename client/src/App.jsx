67d0fc13b348caafdc9cdc1dcb8cd7ebcc5e20f9
samir <mandalsam@outlook.com>
2026-08-02 20:03:38 +0300

    feat(client): add error handling, conditional rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,25 @@ import ApplicationForm from "./ApplicationForm"
 import { useState, useEffect } from "react"
 
 function App() {
-  
+
   const [applications, setApplications] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     async function loadApplications() {
-      const response = await fetch('http://localhost:3000/api/applications')
-      const data = await response.json()
-      setApplications(data)
+      try {
+        const response = await fetch('http://localhost:3000/api/applications')
+        if (!response.ok) {
+          throw new Error(`Server responded ${response.status}`)
+        } 
+        const data = await response.json()
+        setApplications(data)
+      } catch (err) {
+          setError(err.message)
+      } finally {
+          setLoading(false)
+      }
     }
     loadApplications()
   }, [])
@@ -21,6 +32,11 @@ function App() {
       <h1>Job Tracker</h1>
       <p className="tagline">A simple app to track job applications and their status.</p>
       <ApplicationForm />
+
+      {loading && <p>Loading...</p>}
+      {error && <p className="error">Error {error}</p>}
+      {!loading && !error && applications.length === 0 && <p>Application list is empty!</p>}
+
       <div className="card-list">
         {applications.map((application) => <ApplicationCard key={application.id} application={application}/>)}
       </div>
