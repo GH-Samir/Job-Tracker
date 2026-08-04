547769dd87043400ed3882f057b14793d1396c52
samir <mandalsam@outlook.com>
2026-08-04 21:45:57 +0300

    feat(server): remove hardcoded data, fetch database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import db from './db.js'
 
 const app = express()
 
@@ -7,47 +8,14 @@ app.use((req, res, next) => {
   next()
 })
 
-const applications = [
-    {
-      id: 0,
-      company: 'Anthropic',
-      role: 'Software Engineer Intern',
-      dateApplied: '2026-07-12',
-      status: 'PENDING',
-      deadline: '2026-07-17'
-    },
-    {
-      id: 1,
-      company: 'Microsoft',
-      role: 'Data Analyst',
-      dateApplied: '2026-05-02',
-      status: 'OFFER',
-      deadline: '2026-05-17'
-    },
-    {
-      id: 2,
-      company: 'Acturis',
-      role: 'Integrations Developer',
-      dateApplied: '2026-05-19',
-      status: 'REJECTED',
-      deadline: '2026-07-01'
-    },
-    {
-      id: 3,
-      company: 'Google',
-      role: 'Security consultant',
-      dateApplied: '2026-08-11',
-      status: 'PENDING',
-      deadline: '2026-09-24'
-    }
-  ]
-
 app.get('/', (req, res) => {
   res.send("Hello man yeah")
 })
 
+const selectAll = db.prepare('SELECT * FROM applications')
+
 app.get('/api/applications', (req, res) => {
-  res.json(applications)
+  res.json(selectAll.all())
 })
 
 app.listen(3000, () => {console.log("Listening on http://localhost:3000")})
