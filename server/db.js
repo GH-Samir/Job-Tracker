e01a1fa7721926f1de8b1ca774a068c4e1d6b604
samir <mandalsam@outlook.com>
2026-08-04 22:08:12 +0300

    feat(server): add env files

--- server/db.js
+++ server/db.js
@@ -1,7 +1,14 @@
 import Database from 'better-sqlite3'
 
+
+const DB_PATH = (process.env.DB_PATH)
+
+if (!DB_PATH) {
+  throw new Error('Database path cannot be null. Set database path in .env')
+}
+
 // Opens the file, creating it if it doesn't exist yet.
-const db = new Database('job-tracker.db')
+const db = new Database(DB_PATH)
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS applications (
